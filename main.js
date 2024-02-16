209812c78f85b67055a68ae0b702e1b2b70cd614
Amesileo <gheathcollins@gmail.com>
2024-02-16 11:47:36 +0000

    Fixed error on menu animation and introduced a scroll listener to remove the menu on scroll

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let dropdown = document.getElementsByClassName("dropDown")[0];
 let show = 0;
 menu.addEventListener("mousedown", () => {
   show === 0 ? (show = 1) : (show = 0);
-  show === 0 ? (dropdown.style.right = "-100vw")(menu.firstChild.style.transform = "rotate(90deg)") : (dropdown.style.right = "0")(menu.firstChild.style.transform = "rotate(0deg)");
+  show === 0 ? (dropdown.style.right = "-100vw")(menu.children[0].style.transform = "rotate(90deg)") : (dropdown.style.right = "0")(menu.children[0].style.transform = "rotate(0deg)");
 });
 
 menu.addEventListener("touchend", () => {
@@ -15,4 +15,10 @@ let propertiesBtn = document.getElementById("propertiesBtn");
 
 propertiesBtn.addEventListener("click", () => {
   window.location.href = "https://www.onthemarket.com/agents/branch/residential-lettings-specialists-ipswich/properties/?let-agreed=true&search-type=to-rent&view=grid";
+});
+
+document.addEventListener("scroll", () => {
+  dropdown.style.right = "-100vw";
+  menu.children[0].style.transform = "rotate(90deg)";
+  show = 0;
 });
