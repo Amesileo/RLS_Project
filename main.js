e45307325d9228d6c81ec8291ed99e358fc3066f
Amesileo <gheathcollins@gmail.com>
2024-02-16 21:51:07 +0000

    Formatting

--- main.js
+++ main.js
@@ -3,13 +3,19 @@ let dropdown = document.getElementsByClassName("dropDown")[0];
 let show = 0;
 menu.addEventListener("mousedown", () => {
   show === 0 ? (show = 1) : (show = 0);
-  show === 0 ? (dropdown.style.right = "-100vw")(menu.children[0].style.transform = "rotate(90deg)") : (dropdown.style.right = "0")(menu.children[0].style.transform = "rotate(0deg)");
+  show === 0
+    ? (dropdown.style.right = "-100vw")(
+        (menu.children[0].style.transform = "rotate(90deg)")
+      )
+    : (dropdown.style.right = "0")(
+        (menu.children[0].style.transform = "rotate(0deg)")
+      );
 });
 
 menu.addEventListener("touchend", () => {
   menu.style.transform = "scale(1)";
   menu.style.opacity = "1";
-})
+});
 
 let propertiesBtn = document.getElementById("propertiesBtn");
 
@@ -38,12 +44,13 @@ let otmBtn = document.getElementsByClassName("award")[3];
 
 otmBtn.addEventListener("mouseover", () => {
   otmBtn.style.cursor = "pointer";
-})
+});
 
 otmBtn.addEventListener("click", () => {
   goToProperties();
 });
 
 function goToProperties() {
-  window.location.href = "https://www.onthemarket.com/agents/branch/residential-lettings-specialists-ipswich/properties/?let-agreed=true&search-type=to-rent&view=grid";
+  window.location.href =
+    "https://www.onthemarket.com/agents/branch/residential-lettings-specialists-ipswich/properties/?let-agreed=true&search-type=to-rent&view=grid";
 }
